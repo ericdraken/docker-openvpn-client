271dcfaeb19e8ba72f020287a7a00a265899f8bf
Eric Draken <ericdraken@gmail.com>
2019-10-20 13:50:33 -0700

    Remove stale Squid pid (see https://github.com/phusion/baseimage-docker/issues/227)

--- squid/squid-run.sh
+++ squid/squid-run.sh
@@ -2,5 +2,9 @@
 
 # Start Squid
 echo "Starting Squid"
+
+# Prevent stale PID (https://github.com/phusion/baseimage-docker/issues/227)
+rm -rf /var/run/squid.pid
+
 #svwaitup 10 /service/openvpn || exit 1
 exec squid -sN
